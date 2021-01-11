73abed0efa50cdd1739870c958850f3909078558
Sven Stähs <staehs.sven@web.de>
2021-01-11 23:33:56 +0100

    include commit hash in output filename just for information

--- buildroot/share/PlatformIO/scripts/random-bin.py
+++ buildroot/share/PlatformIO/scripts/random-bin.py
@@ -1,5 +1,11 @@
 Import("env")
 
 from datetime import datetime
+from subprocess import Popen, PIPE
 
-env['PROGNAME'] = datetime.now().strftime("firmware-%Y%m%d-%H%M%S")
+# determine commit hash (which isn't sortable, so we append it after the tiemstamp)
+cmd = Popen(['git', 'rev-parse', '--short', 'HEAD'], shell=True, stdout=PIPE)
+for line in cmd.stdout:
+    commit = line.decode().rstrip()
+
+env['PROGNAME'] = datetime.now().strftime("firmware-%Y%m%d-%H%M%S-" + commit)
