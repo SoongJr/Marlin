41d15f5531ff7fe0d837b66977ab505437f0b69d
Sven Stähs <staehs.sven@web.de>
2021-01-11 23:33:56 +0100

    include commit hash in output filename to ensure it gets flashed (see Marlin/README.md)

--- buildroot/share/PlatformIO/scripts/random-bin.py
+++ buildroot/share/PlatformIO/scripts/random-bin.py
@@ -1,9 +1,16 @@
 #
 # random-bin.py
-# Set a unique firmware name based on current date and time
+# Set a unique firmware name based on current date and time plus commit hash
 #
 import pioutil
 if pioutil.is_pio_build():
 	from datetime import datetime
+	from subprocess import Popen, PIPE
 	Import("env")
-	env['PROGNAME'] = datetime.now().strftime("firmware-%Y%m%d-%H%M%S")
+
+	# determine commit hash (which isn't sortable, so we append it after the tiemstamp)
+	cmd = Popen(['git', 'rev-parse', '--short', 'HEAD'], shell=True, stdout=PIPE)
+	for line in cmd.stdout:
+		commit = line.decode().rstrip()
+
+	env['PROGNAME'] = datetime.now().strftime("firmware-%Y%m%d-%H%M%S-" + commit)
